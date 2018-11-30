317b99b6fe48e516a8d7dd5b72c44569972d8035
Birgit <sormusbirgit@gmail.com>
2018-11-30 21:33:59 +0200

    api key

--- kaart.js
+++ kaart.js
@@ -1,6 +1,6 @@
 function initMap() {
   var tartu = {lat: 58.378, lng: 26.729};
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: tartu});
+      document.getElementById('map'), {zoom: 7, center: tartu});
   var marker = new google.maps.Marker({position: tartu, map: map});
 }
